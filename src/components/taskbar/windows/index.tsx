33f41af596c2e48ec214f3651e458e9f88674a7f
Zyggzzz <dolislaegers@gmail.com>
2025-10-24 10:52:17 +0200

    feat: add taskbar window toggle controls

---
+++ src/components/taskbar/windows/index.tsx
@@ -0,0 +1,22 @@
+"use client";
+import { Toggle } from "@components/ui/toggle";
+import { useApps } from "@contexts/AppsContext";
+
+export default function Windows() {
+  const { apps, toggleApp } = useApps();
+
+  return (
+    <div>
+      {apps.map((app) => (
+        <Toggle
+          key={app.id}
+          pressed={app.active}
+          onPressedChange={() => toggleApp(app.id)}
+          className="hover:bg-accent/50 rounded-md"
+        >
+          {app.icon}
+        </Toggle>
+      ))}
+    </div>
+  );
+}
