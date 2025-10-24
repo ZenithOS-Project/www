6634f0b8424c467a7f4b64d412e3ea219107ae93
Zyggzzz <dolislaegers@gmail.com>
2025-10-24 10:56:50 +0200

    fix: rename backgrund image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className="max-h-screen min-h-screen max-w-screen min-w-screen overflow-hidden bg-[url('public/1.jpg')] bg-cover bg-center">
+      <body className="max-h-screen min-h-screen max-w-screen min-w-screen overflow-hidden bg-[url('public/defaultBackground.jpg')] bg-cover bg-center">
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           {children}
           <Toaster position="top-right" swipeDirections={["right"]} />
