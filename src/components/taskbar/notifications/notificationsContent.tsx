edfde8b3b85e6bd0a6982455781652e65d1752ca
Zyggzzz <dolislaegers@gmail.com>
2025-10-24 11:48:43 +0200

    fix: update notifications content component

--- src/components/taskbar/notifications/notificationsContent.tsx
+++ src/components/taskbar/notifications/notificationsContent.tsx
@@ -1,5 +1,5 @@
 "use client";
-import type { Notification } from "@types/notifications";
+import type { Notification } from "@/types";
 import EmptyNotifications from "./empty";
 import { ScrollArea } from "@components/ui/scroll-area";
 import {
